0172430bf3957023636937d2d29aae2d5503028d
loganzzz7 <112505545+loganzzz7@users.noreply.github.com>
2025-06-15 19:10:43 -0500

    done actually

--- neetcode 75/binary search/find_minimum_in_rotated_array.py
+++ neetcode 75/binary search/find_minimum_in_rotated_array.py
@@ -30,12 +30,12 @@ class Solution:
         r = len(nums) - 1
         while l <= r:
             if nums[l] < nums[r]:
-                # is alr sorted
-                result = min(nums[l], result)
+                result = min(result, nums[l])
                 break
-            
+
             mid = (l + r) // 2
             result = min(result, nums[mid])
+
             if nums[l] <= nums[mid]:
                 l = mid + 1
             else:
